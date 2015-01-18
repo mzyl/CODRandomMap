c4ec991069acc9809736471c5eb4cba6fa1d8afd
mzyl <maxzylstra>
2015-01-17 23:43:06 -0600

    Third Commit

--- app/src/main/java/com/app/max/codrandommap/MainActivity.java
+++ app/src/main/java/com/app/max/codrandommap/MainActivity.java
@@ -1,9 +1,18 @@
 package com.app.max.codrandommap;
 
+import android.graphics.Paint;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +21,58 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        final TextView textView2 = (TextView) findViewById(R.id.textView2);
+        final TextView textView3 = (TextView) findViewById(R.id.textView3);
+        Button button_1 = (Button) findViewById(R.id.generate_button);
+
+        final List<String> mapList = new ArrayList<>();
+        mapList.add("Aftermath");
+        mapList.add("Cargo");
+        mapList.add("Carrier");
+        mapList.add("Drone");
+        mapList.add("Express");
+        mapList.add("Hijacked");
+        mapList.add("Meltdown");
+        mapList.add("Overflow");
+        mapList.add("Plaza");
+        mapList.add("Raid");
+        mapList.add("Slums");
+        mapList.add("Standoff");
+        mapList.add("Turbine");
+        mapList.add("Yemen");
+
+        final List<String> played = new ArrayList<>();
+        final Random i = new Random();
+
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final int rand = i.nextInt(mapList.size());
+
+                textView.setText(mapList.get(rand));
+                played.add(mapList.get(rand));
+                mapList.remove(mapList.get(rand));
+
+                Collections.sort(played, String.CASE_INSENSITIVE_ORDER);
+
+                StringBuilder map = new StringBuilder();
+
+                for (String s : played){
+                    map.append(" ");
+                    map.append(s);
+                    map.append(" \t");
+                }
+
+                String count;
+                count = Integer.toString(mapList.size());
+
+                textView2.setPaintFlags(textView2.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                textView2.setText(map);
+                textView3.setText(count);
+            }
+        });
     }
 
 
